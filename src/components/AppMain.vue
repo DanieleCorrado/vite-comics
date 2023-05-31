271e5740ac20464b58163e1a4cfd1d97c1d72014
DanieleCorrado <corrado_daniele@virgilio.it>
2023-05-31 17:39:26 +0200

    add main

---
+++ src/components/AppMain.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: "AppMain",
+}
+</script>
+
+
+<template>
+
+<main>
+  <div id="content">
+  <span>Content goes here...</span>
+  </div>
+</main>
+
+</template>
+
+<style lang="scss" scoped>
+
+@use '../styles/partials/variables.scss' as *;
+
+main {
+  color: white;
+
+  #content {
+    width: 80%;
+    margin: 0 auto;
+    padding: 30px;
+  }
+  
+}
+
+</style>
